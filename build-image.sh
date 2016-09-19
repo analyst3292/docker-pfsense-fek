fc9866e5326a3e6ad4c155566b819a9c32fb41bb
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-19 23:45:35 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -6,13 +6,13 @@ sleep 5s
 
 docker-compose stop
 docker-compose rm -f
-docker images | grep ^fluentdelasticsearchkibana | awk '{print $3}' | xargs docker rmi
+docker images | grep ^docker-pfsense-elk | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
-docker build -t fluentd ./fluentd
-docker build -t nginx ./nginx
+docker build -t logstash ./logstash
 
-docker-compose up -d elasticsearch fluentd kibana
-docker-compose up -d nginx1
+
+docker-compose up -d elasticsearch logstash kibana
+
 
 docker-compose logs -f
