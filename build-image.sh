04173abec159712ce4c90bebfbd0cd4642903d9c
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-22 20:25:36 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ sleep 5s
 
 docker-compose stop
 docker-compose rm -f
-docker images | grep ^dockerpfsenseelk | awk '{print $3}' | xargs docker rmi
+docker images | grep ^dockerpfsensefek | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
 docker-compose up -d
