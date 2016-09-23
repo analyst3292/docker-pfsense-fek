984a1e3550ecdedcb2e7dc690f904a47775a529c
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-23 13:34:52 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ sleep 5s
 
 docker-compose stop
 docker-compose rm -f
-docker images | grep ^dockerpfsensefek | awk '{print $3}' | xargs docker rmi
+docker images | grep ^dockerpfsensefek_logstash | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
 docker build --no-cache -t dockerpfsensefek_logstash logstash
