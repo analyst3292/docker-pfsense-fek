aadaced09d07a0b62d5a6dca5966c5a621294392
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-23 13:36:08 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -5,7 +5,7 @@ git pull
 sleep 5s
 
 docker-compose stop
-docker-compose rm -f
+docker-compose rm -f logstash
 docker images | grep ^dockerpfsensefek_logstash | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
