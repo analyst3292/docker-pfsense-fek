38e2e77ce4177668fc87493500ed622afdb10599
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-24 12:00:15 +0900

    fix timezone

--- logstash/5.0.0/docker-entrypoint.sh
+++ logstash/5.0.0/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e
+CONFIG_FILE=/etc/logstash/conf.d/11-pfsense.logstash.conf
+
+if [ -n "$NODE_RACK" ];then
+    sed -e '/^      timezone => "UTC"/c       timezone => "$TIMEZONE"' \
+        -i.bk $CONFIG_FILE
+fi
+
 
 # Add logstash as command if needed
 if [ "${1:0:1}" = '-' ]; then
