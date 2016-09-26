18869e4cb75959bf647c816b6f9f5f82f4531d02
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-26 21:43:13 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -4,23 +4,13 @@ git pull
 
 sleep 5s
 
-#docker-compose stop
-#docker-compose rm -f fluent elasticsearch
-#docker images | grep ^dockerpfsensefek_fluent | awk '{print $3}' | xargs docker rmi
-#docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
-
-#docker build --no-cache -t dockerpfsensefek_logstash logstash
-
-#docker-compose rm -f
-#docker-compose up
-
+docker-compose stop
+docker-compose rm -f fluent elasticsearch
 docker images | grep ^dockerpfsensefek_fluent | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
-docker build -t dockerpfsensefek_fluent fluent
+docker build --no-cache -t dockerpfsensefek_logstash logstash
 
-docker run --rm -it \
-	--name fl \
-	-p "5514:5514/udp" \
-	-e TIMEZONE="Asia/Tokyo" \
-	dockerpfsensefek_fluent
+docker-compose rm -f
+docker-compose up
+
