81b69fa270e16ffce1a354d7c6753ef728586316
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-27 21:15:34 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ sleep 5s
 
 docker-compose stop
 docker-compose rm -f fluent elasticsearch
-#docker images | grep ^dockerpfsensefek_fluent | awk '{print $3}' | xargs docker rmi
+docker images | grep ^dockerpfsensefek_fluent | awk '{print $3}' | xargs docker rmi
 docker images | grep ^'<none>' | awk '{print $3}' | xargs docker rmi
 
 docker build --no-cache -t dockerpfsensefek_logstash logstash
