6ece6c0506422616e06114b6b87596b04f2bf382
FoxBoxsnet <naokiaoyama2009@gmail.com>
2016-10-04 14:25:03 +0900

    fix

--- fluent/docker-entrypoint.sh
+++ fluent/docker-entrypoint.sh
@@ -3,9 +3,10 @@
 #// docker-entrypoint.sh
 #//==============================================
 
-#MAPPING_FILE=/fluentd/etc/pfsense-mapping.json
+MAPPING_FILE=/fluentd/etc/conf.d/pfsense-mapping.json
 
-#	curl -X PUT elasticsearch:9200/_template/pfsense --data @$MAPPING_FILE
+curl -X PUT elasticsearch:9200/_template/pfsense --data @$MAPPING_FILE
+curl -X GET elasticsearch:9200/_template/pfsense
 
 #//==============================================
 #// start fluentd
