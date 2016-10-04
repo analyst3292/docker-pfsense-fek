22fe7a629a333743395b2c2ad6c1339197fec6a2
foxboxsnet <naokiaoyama2009@gmail.com>
2016-10-04 18:56:36 +0900

    fix

--- fluent/docker-entrypoint.sh
+++ fluent/docker-entrypoint.sh
@@ -5,6 +5,8 @@
 
 MAPPING_FILE=/fluentd/etc/pfsense-mapping.json
 
+echo "Sleep 30s"
+Sleep 30
 echo "MAPPING : $MAPPING_FILE"
 curl -X PUT elasticsearch:9200/_template/pfsense --data @$MAPPING_FILE
 curl -X GET elasticsearch:9200/_template/pfsense
